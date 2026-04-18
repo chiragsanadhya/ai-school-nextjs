a7252bffb1377f86ede76c99732d34bdefdd9f1e
chiragsanadhya <chiragsanadhya1411@gmail.com>
2026-04-18 14:18:17 +0530

    final build

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,18 +1,23 @@
-import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg"
+import { PrismaClient } from "@prisma/client"
+import { Pool } from "pg"
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
-};
+  prisma: PrismaClient | undefined
+}
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+})
 
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    adapter: new PrismaPg(process.env.DATABASE_URL!),
-  });
+    adapter: new PrismaPg(pool),
+  })
 
 if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = prisma;
+  globalForPrisma.prisma = prisma
 }
 
-export default prisma;
+export default prisma
